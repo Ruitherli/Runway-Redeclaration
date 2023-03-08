18ec7a056e31e18a337f9f27588f0e2f054674ca
yasink <nkYASMIN@YAHOO.COM>
2023-03-08 11:11:53 +0800

    changed the names of the javafx controller and main

--- src/main/java/com/example/runwayproject/UIController.java
+++ src/main/java/com/example/runwayproject/UIController.java
@@ -3,7 +3,7 @@ package com.example.runwayproject;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class UIController {
     @FXML
     private Label welcomeText;
 
