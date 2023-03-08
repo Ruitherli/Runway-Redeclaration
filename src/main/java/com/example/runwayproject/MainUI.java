497f5d363ea22ff7ee3a0254cc46b70364d03f79
yasink <nkYASMIN@YAHOO.COM>
2023-03-08 15:22:00 +0800

    create two fxml pages: one for entering obstacle info and one for viewing the calculations The only working button is the calculate button which only goes from the obstacle page to the calculation page.

--- src/main/java/com/example/runwayproject/MainUI.java
+++ src/main/java/com/example/runwayproject/MainUI.java
@@ -11,8 +11,8 @@ public class MainUI extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainUI.class.getResource("obstacle.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        stage.setTitle("Obstacle Information");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/example/runwayproject/UIController.java
+++ src/main/java/com/example/runwayproject/UIController.java
@@ -1,14 +1,51 @@
 package com.example.runwayproject;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.SocketTimeoutException;
+
 
 public class UIController {
+    //tB == text box
     @FXML
-    private Label welcomeText;
+    private TextField tBdistanceFromThreshold;
+    @FXML
+    private TextField tBdistanceFromCenterline;
+    @FXML
+    private TextField tBobstacleHeight;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+
+    public void obstacleInfo() {
+
+        //Integer distanceFromThreshold = Integer.valueOf(tBdistanceFromThreshold.getText());
     }
-}
+
+    public void goToCalculatePage() throws IOException {
+
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("calculation.fxml"));
+            Stage stage = new Stage();
+            Scene scene = new Scene(root);
+            stage.setTitle("Breakdown Of Calculations");
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException ei) {
+            ei.printStackTrace();
+        }
+    }
+
+    public void calculate(ActionEvent event) throws IOException {
+           goToCalculatePage();
+        }
+    }
