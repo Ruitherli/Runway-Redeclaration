1fc9891744af6f3c8aa77d1e0e7ab70010fcdda2
Heng Rui Kang <ruikang2002@hotmail.com>
2023-03-10 17:55:40 +0800

    Set table column to unresizable and unreordable Set splitplane to unresizable

--- src/main/java/com/example/runwayproject/Controller/SceneController.java
+++ src/main/java/com/example/runwayproject/Controller/SceneController.java
@@ -6,6 +6,10 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Control;
+import javafx.scene.control.SplitPane;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -21,6 +25,14 @@ public class SceneController implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
     }
+
+    public void formatTable(TableView<?> table){
+        for (TableColumn column : table.getColumns()) {
+            column.setResizable(false);
+            column.setReorderable(false);
+        }
+    }
+
     public void selectScene(ActionEvent event,String fxml) throws IOException{
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/com/example/runwayproject/"+fxml)));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
