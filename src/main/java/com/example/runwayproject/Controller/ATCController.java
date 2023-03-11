01ea26c8049de29372922c8317571b9efb1c9fdb
Heng Rui Kang <ruikang2002@hotmail.com>
2023-03-11 20:38:09 +0800

    Added more features to ATC page

--- src/main/java/com/example/runwayproject/Controller/ATCController.java
+++ src/main/java/com/example/runwayproject/Controller/ATCController.java
@@ -1,23 +1,70 @@
 package com.example.runwayproject.Controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableView;
+import javafx.scene.text.Text;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ATCController extends SceneController{
+
+    @FXML
+    private Text alsText;
+
+    @FXML
+    private Text blastProtectionText;
+
+    @FXML
+    private Text centerlineText;
+
+    @FXML
+    private Text directionText;
+
+    @FXML
+    private Text heightText;
+
+    @FXML
+    private Text lengthText;
+
+    @FXML
+    private Text nameText;
+
     @FXML
     private TableView<?> oriDistanceTable;
 
     @FXML
     private TableView<?> recDistanceTable;
 
+    @FXML
+    private Text resaText;
+
+    @FXML
+    private ComboBox<?> runwayComboBox;
+
+    @FXML
+    private TableView<?> runwayTable;
+
+    @FXML
+    private Text stripEndText;
+
+    @FXML
+    private Text thresholdLText;
+
+    @FXML
+    private Text thresholdRText;
+
+    @FXML
+    private Text tocsText;
+
+    @FXML
+    private Text widthText;
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         formatTable(oriDistanceTable);
         formatTable(recDistanceTable);
-
+        formatTable(runwayTable);
     }
 
 }
